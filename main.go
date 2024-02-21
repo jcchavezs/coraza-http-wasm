ef2645a8fff76be72f6fc33edbed5d478009c5f3
José Carlos Chávez <jcchavezs@gmail.com>
2024-02-21 23:14:01 +0100

    chore: upgrades http-wasm host and guest.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/corazawaf/coraza-http-wasm/operators"
@@ -167,12 +168,14 @@ func handleRequest(req api.Request, res api.Response) (next bool, reqCtx uint32)
 		client string
 		cport  int
 	)
+
 	// IMPORTANT: Some http.Request.RemoteAddr implementations will not contain port or contain IPV6: [2001:db8::1]:8080
-	// idx := strings.LastIndexByte(req.RemoteAddr, ':')
-	// if idx != -1 {
-	//	 client = req.RemoteAddr[:idx]
-	//	 cport, _ = strconv.Atoi(req.RemoteAddr[idx+1:])
-	// }
+	srcAddress := req.GetSourceAddr()
+	idx := strings.LastIndexByte(srcAddress, ':')
+	if idx != -1 {
+		client = srcAddress[:idx]
+		cport, _ = strconv.Atoi(srcAddress[idx+1:])
+	}
 
 	var it *types.Interruption
 	// There is no socket access in the request object, so we neither know the server client nor port.
